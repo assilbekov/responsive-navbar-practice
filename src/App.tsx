fab2361598b7f47655945fea44371f1f84c3851c
assilbekov <askhat.assilbekov@gmail.com>
2024-05-03 14:21:55 -0300

    add logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,33 @@ import "./app.css";
 
 function App() {
   return (
-    <nav>
-      <ul id="primary-navigation" className="flex primary-navigation">
-        <li className="active">
-          <a href="#index.html">
-            <span aria-hidden="true">00</span> Home
-          </a>
-        </li>
-        <li className="active">
-          <a href="#index.html">
-            <span aria-hidden="true">01</span> Destination
-          </a>
-        </li>
-        <li className="active">
-          <a href="#index.html">
-            <span aria-hidden="true">02</span> Crew
-          </a>
-        </li>
-        <li className="active">
-          <a href="#index.html">
-            <span aria-hidden="true">03</span> Technology
-          </a>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <div>LOGO</div>
+      <nav>
+        <ul id="primary-navigation" className="flex primary-navigation">
+          <li className="active">
+            <a href="#index.html">
+              <span aria-hidden="true">00</span> Home
+            </a>
+          </li>
+          <li className="active">
+            <a href="#index.html">
+              <span aria-hidden="true">01</span> Destination
+            </a>
+          </li>
+          <li className="active">
+            <a href="#index.html">
+              <span aria-hidden="true">02</span> Crew
+            </a>
+          </li>
+          <li className="active">
+            <a href="#index.html">
+              <span aria-hidden="true">03</span> Technology
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
