26deb38f3e30f2f4556e45bc42529bec587a4a28
assilbekov <askhat.assilbekov@gmail.com>
2024-05-03 14:16:05 -0300

    add basic markup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import { useState } from 'react'
+import "./app.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    </>
-  )
+    <nav>
+      <ul id="primary-navigation" className="flex primary-navigation">
+        <li className="active">
+          <a href="#index.html">
+            <span aria-hidden="true">00</span> Home
+          </a>
+        </li>
+        <li className="active">
+          <a href="#index.html">
+            <span aria-hidden="true">01</span> Destination
+          </a>
+        </li>
+        <li className="active">
+          <a href="#index.html">
+            <span aria-hidden="true">02</span> Crew
+          </a>
+        </li>
+        <li className="active">
+          <a href="#index.html">
+            <span aria-hidden="true">03</span> Technology
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default App
+export default App;
